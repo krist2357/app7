7eeb8de1705c2da185d0fa05015f413911393522
Christian Cadena <75154211+krist2357@users.noreply.github.com>
2023-07-10 22:01:33 -0500

    Creación de funcion backend

---
+++ backend.py
@@ -0,0 +1,20 @@
+import requests
+API_KEY = "5d8990e912117488270a09e634383792"
+
+def get_data(place, forecast_days, kind):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place},ec&APPID={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data['list']
+    #Aqui, si se ve la longitud de los datos filtrados. Dan 40. esto quiere decir que en 5 dias se han realizado
+    #8observaciones en un día
+    nr_values = 8*forecast_days # 8 observaciones
+    filtered_data = filtered_data[:nr_values]
+    if kind == 'Temperature':
+        filtered_data = [dict['main']['temp'] for dict in filtered_data] # de igual forma es el contenido de la url
+    if kind == 'Sky':
+        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    print(get_data(place="Quito",forecast_days=3, kind='Temperature'))
--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
 st.title('Weather Forecast for the next days')
-place = st.text_input('Place: ')
+place = st.text_input('Place in Ecuador: ')
 days = st.slider('Forescats Days', min_value=1, max_value=5, help='Select the number the forecast days')
 option = st.selectbox('Select data to view',('Temperature','Sky'))
 st.subheader(f'{option} for the next {days} days in {place}')
 
-# genero  una gráfica con los siguientes datos
-dates = ['2022-25-10', '2022-26-10', '2022-27-10']
-temperatures = [10, 11, 15]
-#cambio la escala en y con el slider
-temperatures = [days*i for i in temperatures]
+d, t = get_data(place, days, option)
 
-figure = px.line(x=dates, y=temperatures, labels={'x':'Date', 'y':'Temperature (C)'})
+d, t = get_data(days)
+
+figure = px.line(x=d, y=t, labels={'x':'Date', 'y':'Temperature (C)'})
 st.plotly_chart(figure)
