f478293ac5af23577ceb2c0ed73e41fcf3ce02ab
Christian Cadena <75154211+krist2357@users.noreply.github.com>
2023-07-17 16:52:44 -0500

    Terminar programa en backend y en el main.

--- backend.py
+++ backend.py
@@ -1,20 +1,16 @@
 import requests
 API_KEY = "5d8990e912117488270a09e634383792"
 
-def get_data(place, forecast_days, kind):
+def get_data(place, forecast_days):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place},ec&APPID={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     #Aqui, si se ve la longitud de los datos filtrados. Dan 40. esto quiere decir que en 5 dias se han realizado
-    #8observaciones en un día
+    #8 observaciones en un día
     nr_values = 8*forecast_days # 8 observaciones
-    filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data] # de igual forma es el contenido de la url
-    if kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+    filtered_data = filtered_data[:nr_values] 
     return filtered_data
 
 if __name__ == "__main__":
-    print(get_data(place="Quito",forecast_days=3, kind='Temperature'))
+    print(get_data(place="Quito",forecast_days=3))
--- main.py
+++ main.py
@@ -2,15 +2,33 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
-st.title('Weather Forecast for the next days')
-place = st.text_input('Place in Ecuador: ')
-days = st.slider('Forescats Days', min_value=1, max_value=5, help='Select the number the forecast days')
-option = st.selectbox('Select data to view',('Temperature','Sky'))
-st.subheader(f'{option} for the next {days} days in {place}')
+st.title('Pronostico del tiempo para los siguientes días')
+place = st.text_input('Lugar: ')
+days = st.slider('Pronostico del tiempo', min_value=1, max_value=5, help='Seleccione el número de días.')
+option = st.selectbox('Select data to view',('Temperatura','Cielo'))
+st.subheader(f'{option} para los {days} siguientes días en {place}')
 
-d, t = get_data(place, days, option)
+if place:
+    try:
+        # Traer los datos de temperatura y cielo.
+        filtered_data = get_data(place, days)
 
-d, t = get_data(days)
+        if option == 'Temperatura':
+            #Se divide para 10 ya que no muestra bien los datos de temperatura
+            temperatures = [dict['main']['temp']/10 for dict in filtered_data]
+            dates = [dict['dt_txt'] for dict in filtered_data]
+            # Crear el grafico de temperatura.
+            figure = px.line(x=dates, y=temperatures, labels={'x':'Date', 'y':'Temperature (C)'})
+            st.plotly_chart(figure)
 
-figure = px.line(x=d, y=t, labels={'x':'Date', 'y':'Temperature (C)'})
-st.plotly_chart(figure)
+        if option == 'Cielo':
+            # se genera, ya que de la data que sale con la API, sale, Clear, Clouds, etc...
+            images = {'Clear':'images/clear.png','Clouds':'images/cloud.png','Rain':'images/rain.png','Snow':'images/snow.png'}
+            sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+            #Genero un lazo for para todas las condiciones que bote la lsita que trae la API.
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            # Una imagen por condición
+            st.image(image_paths, width=115)
+    except:
+        st.write('El lugar no existe en la Base de datos.')
